f7ce2cda3ac1a768e6813cce18140a6cce82311e
MOCH EKO BUDI SETIAWAN <gseqoo@gmail.com>
2022-07-14 00:48:45 +0700

    edit welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
       
         <div class="p-10 border rounded-xl bg-gray-100 text-center">
             <h1 class="font-semibold text-2xl m-2">Join With Us</h1>
-            <div class="text-lg text-gray-600 mb-4">Best Social Media New By Eko Budi</div>
+            <div class="text-lg text-gray-600 mb-4">Semoga Lolos Di Ngopibareng.id</div>
             <a href="{{ route('register') }}" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-xl font-semibold text-sm text-white capitalize tracking-widest hover:bg-blue-700 active:bg-blue-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150"> Register</a>
             <a href="{{ route('login') }}" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-xl font-semibold text-sm text-white capitalize tracking-widest hover:bg-blue-700 active:bg-blue-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150"> Login</a>
         </div>
