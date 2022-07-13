0163ead420bd65ee917819b5c5dce526243eed46
MOCH EKO BUDI SETIAWAN <gseqoo@gmail.com>
2022-07-14 00:45:43 +0700

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->hasStatuses(5)->count(10)->create();
+        User::factory()->count(100000)->create();
     }
 }
